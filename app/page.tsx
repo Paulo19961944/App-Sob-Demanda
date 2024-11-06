00947b407809e45b6094a1195589d20b6ba4b495
Paulo Henrique <paulo24041996@gmail.com>
2024-11-05 23:46:10 -0300

    feature: Add Footer Component

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 "use client"
 import Title from "../components/title";
 import Main from "../components/main";
+import Footer from "../components/footer";
 
 export default function Page() {
     return (
         <div>
             <Title/>
             <Main/>
+            <Footer/>
         </div>
     );
 }
---
+++ components/footer.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+import styles from '../app/styles.module.css';
+
+export default function Footer(){
+    return(
+        <footer className={styles.footer}>
+            <small className={styles.footerText}>Copyright 2024 - PH Site Developer</small>
+        </footer>
+    )
+}
